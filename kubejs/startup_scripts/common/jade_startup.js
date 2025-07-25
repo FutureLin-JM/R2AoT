8146c63a8d5a85676682ab79ed607d01a2289cac
FutureLin-JM <74280851@qq.com>
2025-07-25 16:25:21 +0800

    修复小bug

--- kubejs/startup_scripts/common/jade_startup.js
+++ kubejs/startup_scripts/common/jade_startup.js
@@ -14,12 +14,8 @@ JadeEvents.onCommonRegistration((event) => {
     event.blockDataProvider('r2aot:numerical_source', $BlockEntity).setCallback((tag, accessor) => {
         const { blockEntity, block } = accessor;
 
-        if (blockEntity.source != null) {
+        if (blockEntity.source != null && !isNaN(blockEntity.source)) {
             tag.putInt('source', blockEntity.source);
-        }
-
-        if (block.id == 'r2aot:source_flower' || block.id == 'r2aot:flowing_source_flower') {
-            tag.putInt('source', blockEntity.persistentData.getInt('source'));
         }
     });
 });
