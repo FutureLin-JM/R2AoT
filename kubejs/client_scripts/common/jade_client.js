f3f6eb0bba6c3dfe32b4d7b2f54c976dc5eac89b
FutureLin-JM <74280851@qq.com>
2025-11-16 00:48:25 +0800

    雪傀儡冷却室兼容Jade

--- kubejs/client_scripts/common/jade_client.js
+++ kubejs/client_scripts/common/jade_client.js
@@ -1,3 +1,5 @@
+const $IElementHelper = Java.loadClass('snownee.jade.api.ui.IElementHelper');
+
 JadeEvents.onClientRegistration(event => {
     event.block('r2aot:numerical_mana', $Block).tooltip((tooltip, accessor) => {
         const { serverData } = accessor;
@@ -20,6 +22,25 @@ JadeEvents.onClientRegistration(event => {
         if (source) {
             let sourceText = Text.translate('jade.tooltip.source', source).lightPurple();
             tooltip.add(sourceText);
+        }
+    });
+
+    event.block('r2aot:snowman_cooler', $Block).tooltip((tooltip, accessor) => {
+        const { serverData } = accessor;
+        if (!serverData) return;
+        let activeFuel = serverData.get('activeFuel');
+        let remainingBurnTime = serverData.get('remainingBurnTime');
+        let activeFuelIcon;
+        if (activeFuel && remainingBurnTime) {
+            if (activeFuel == 1) {
+                activeFuelIcon = $IElementHelper.get().smallItem(Items.SNOWBALL);
+            } else if (activeFuel == 2) {
+                activeFuelIcon = $IElementHelper.get().smallItem(Items.ICE);
+            }
+            tooltip['add(snownee.jade.api.ui.IElement)'](activeFuelIcon);
+            let seconds = Math.floor(remainingBurnTime / 20);
+            let coolerText = Text.translatable('jade.tooltip.snowman_cooler', seconds);
+            tooltip.append(coolerText);
         }
     });
 });
--- kubejs/startup_scripts/common/jade_startup.js
+++ kubejs/startup_scripts/common/jade_startup.js
@@ -1,3 +1,5 @@
+const $SnowmanCoolerBlockEntity = Java.loadClass('fr.iglee42.cmr.cooler.SnowmanCoolerBlockEntity');
+
 JadeEvents.onCommonRegistration(event => {
     const manaMethods = ['currentMana', 'maxMana', 'mana', 'manaToGet'];
 
@@ -16,6 +18,17 @@ JadeEvents.onCommonRegistration(event => {
 
         if (blockEntity.source != null && !isNaN(blockEntity.source)) {
             tag.putInt('source', blockEntity.source);
+        }
+    });
+
+    event.blockDataProvider('r2aot:snowman_cooler', $BlockEntity).setCallback((tag, accessor) => {
+        /** @type {{ blockEntity: Internal.SnowmanCoolerBlockEntity, block: Internal.Block  }} */
+        const { blockEntity, block } = accessor;
+        if (block.id !== 'cmr:snowman_cooler') return;
+
+        if (blockEntity.activeFuel && blockEntity.remainingBurnTime) {
+            tag.putInt('activeFuel', blockEntity.activeFuel.ordinal());
+            tag.putInt('remainingBurnTime', blockEntity.remainingBurnTime);
         }
     });
 });
